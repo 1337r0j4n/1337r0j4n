3fb7fd962fa9b6030ddd23e1aa197417d2a72952
User of Github <84887432+1337r0j4n@users.noreply.github.com>
2024-04-21 22:58:09 +0630

    Add files via upload

---
+++ FILE5/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacked By 1337r0j4n</title>
+    <style>
+    body { background: #000; font-family: 'Trebuchet MS', sans-serif; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-weight: bold; }
+        .mainContainer { text-align: center; text-shadow: 2px 2px 2px #000; }
+        h1 { font-family: 'Josefin Sans', sans-serif; color: #fff; text-shadow: 0 0 10px #fff; margin: 0; }
+        h1:hover { text-shadow: 0 0 14px #fefffe; }
+        .teamName { text-transform: uppercase; letter-spacing: 20px; font-family: 'Josefin Sans', sans-serif; color: #f00; font-size: 17px; }
+        .teamName span { color: #fff; }
+        .hr { width: 600px; border: 2px solid #f00; margin: 10px auto; }
+        .mainImage { margin: 20px 0; width: 200px; border: 3px solid rgba(0, 0, 0, 0.774); border-radius: 50%; box-shadow: 0 0 17px #000; }
+        .mainImage:hover { box-shadow: 0 0 19px #000; }
+        .text1, .text3 { text-transform: uppercase; color: #f00; }
+        .text2 { text-transform: uppercase; color: #fff; }
+</style>	
+</head>
+<body>
+    <div class="mainContainer" id="mainContainer">
+        <div class="img"><img class="mainImage" src="https://archive.ph/Jcrv2/a9a7aecc1027d425a08f6c5a94bf765610e465c4.png" alt="image"></div>
+        <h1>HACKED BY 1337r0j4n | LAPSUS$ Group™</h1>
+        <hr class="hr">
+        <div class="container">
+            <p class="text1">Woops! Hacked? Improve your site's security.</p>
+            <p class="text2">#FreePalestine | #FuckIsrael | #FrenchHackers | #LAPSUS$ | #SMS-1337 | #HACKFORCE</p>
+            <p class="text3">and of course dedication to all the members of HACKFORCE ;)</p>
+            <p class="text2">https://t.me/GroupLapsus</p>
+            <p class="text3">https://twitter.com/LapsusGroup</p>
+        </div>
+    </div>
+</body>
+</html>
+
